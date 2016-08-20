484fb9a42e337fd5570bd87c8f16a108cce2cefc
ponzanaja <ponza2539@hotmail.com>
2016-08-20 16:14:05 +0700

    Poke Beta

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 new Vue({
   el: '#app',
   data: {
-    msg: 'Hello Vue.js'
-  }
+    msg: 'Pokemon Ranger',
+    list: []
+  },
+  methods : {
+    getAPI: function(){
+      var url = 'http://www.pokesnipers.com/api/v1/pokemon.json'
+      var that = this
+      this.$http.get(url).then(function (res) {
+        that.list = res.data.results
+        console.log(that.list);
+      })
+    }
+  },
+    ready: function(){
+      this.getAPI()
+      var that = this
+      setInterval(function(){
+        that.getAPI()
+        console.log('อิอิส์')
+      },400 )
+}
+  })
 
-})
+console.log(app);
